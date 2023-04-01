089d9ce1b933f32cde11661877d0c7430810aca5
Fakorede Damilola <dammy.fakorede@gmail.com>
2023-04-01 18:56:25 +0100

    created the sign in page

---
+++ src/components/SideNav.tsx
@@ -0,0 +1,13 @@
+import "../styles/components.scss"
+
+const SideNav = () => {
+  return (
+    <div className="sidenav">
+      <h2>Scrollable Sidebar</h2>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam euismod mi at urna vestibulum, eget interdum arcu pellentesque. Donec vitae orci justo. Mauris malesuada enim a dolor pretium, eget pharetra nisi hendrerit. Sed vel elit elit. Sed lacinia sodales felis in lacinia. Nullam malesuada neque id efficitur consequat. Nam fermentum, quam vel euismod accumsan, tortor purus placerat ex, vel malesuada purus metus eu velit. </p>
+      <p>Etiam molestie rhoncus magna vitae ullamcorper. Suspendisse tincidunt lectus sit amet nibh lacinia, a tristique ex imperdiet. Sed vitae turpis nec dolor euismod vehicula. Nulla sed semper velit, eget euismod orci. Ut eu ante sit amet dolor laoreet auctor vel sit amet dui. Duis varius diam vitae diam pretium congue. </p>
+    </div>
+  )
+}
+
+export default SideNav
